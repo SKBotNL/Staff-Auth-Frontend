b862a617b5846980931901f06f6b26107d10ebda
Sven <skbotnl@disroot.org>
2026-06-07 22:47:37 +0200

    Tweak 404 page

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -3,8 +3,8 @@ import NotFoundIcon from "~icons/tabler/error-404";
 export default function NotFound() {
   return (
     <div class="flex flex-col min-h-screen items-center justify-center">
-      <NotFoundIcon class="h-16 w-full mb-8" />
-      <p class="text-xl text-center">Not Found</p>
+      <NotFoundIcon class="h-16 w-full" />
+      <p class="text-xl text-center font-bold">Not Found</p>
     </div>
   );
 }
