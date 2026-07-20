68a05d1eeaeeaf83bb4460b437faf58a894334fc
Sven <skbotnl@disroot.org>
2026-07-20 23:56:24 +0200

    Add login button to successful setup page

--- src/pages/successfulSetup.tsx
+++ src/pages/successfulSetup.tsx
@@ -1,4 +1,5 @@
 import CheckIcon from "~icons/mdi/check";
+import { LOGIN_URL } from "../lib/api";
 import { useI18n } from "../providers/I18nProvider";
 
 export default function SuccessfulSetupPage() {
@@ -8,6 +9,13 @@ export default function SuccessfulSetupPage() {
     <div class="flex flex-col min-h-screen items-center justify-center">
       <CheckIcon class="h-16 w-full mb-8" />
       <p class="text-xl text-center">{t("setup.success")}</p>
+      <button
+        type="button"
+        onClick={() => (window.location.href = LOGIN_URL)}
+        class="btn btn-soft mt-4"
+      >
+        {t("login.login")}
+      </button>
     </div>
   );
 }
